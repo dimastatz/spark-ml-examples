2cec0f87d8691359ee4d488b9b900841bdd2b8ba
Dima Statz <dima.statz@gmail.com>
2023-01-29 21:53:15 +0200

    add tests

---
+++ lead-scoring/src/main/scala/com/dstatz/leadscoring/Boot.scala
@@ -0,0 +1,7 @@
+package com.dstatz.leadscoring
+
+object Boot {
+  def main(args: Array[String]): Unit = {
+    println("lead-scoring is started")
+  }
+}
---
+++ lead-scoring/src/test/scala/com/dstatz/leadscoring/tests/TestDataCollector.scala
@@ -0,0 +1,9 @@
+package com.dstatz.leadscoring.tests
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class TestDataCollector extends AnyFunSuite {
+  test("testSimpleTest") {
+    assert(1 == 1)
+  }
+}
