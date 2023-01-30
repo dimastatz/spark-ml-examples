c241223f1d9a2d05651798f9f1be8118ffdedf93
Dima Statz <dima.statz@gmail.com>
2023-01-30 13:37:30 +0200

    use logging

--- lead-scoring/src/main/scala/com/dstatz/leadscoring/Boot.scala
+++ lead-scoring/src/main/scala/com/dstatz/leadscoring/Boot.scala
@@ -1,7 +1,13 @@
 package com.dstatz.leadscoring
 
+import org.apache.log4j.LogManager
+import org.apache.spark.sql.SparkSession
+
 object Boot {
+  private lazy val log = LogManager.getLogger(Boot.getClass)
   def main(args: Array[String]): Unit = {
     println("lead-scoring is started")
+    log.info("creating spark session")
+    val session = SparkSession.builder.getOrCreate()
   }
 }
