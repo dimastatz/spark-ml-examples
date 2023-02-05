b48eaa4385a53ee48086d18bfc3a374a0df6c30d
Dima Statz <dima.statz@gmail.com>
2023-02-05 22:14:58 +0200

    add spark app

--- lead-scoring/src/main/scala/com/dstatz/leadscoring/Boot.scala
+++ lead-scoring/src/main/scala/com/dstatz/leadscoring/Boot.scala
@@ -1,21 +1,23 @@
 package com.dstatz.leadscoring
 
+import com.typesafe.config._
 import org.apache.log4j.LogManager
 import com.dstatz.leadscoring.common._
-import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.SparkSession
 
 object Boot {
   private lazy val log = LogManager.getLogger(Boot.getClass)
+
   def main(args: Array[String]): Unit = {
     import com.dstatz.leadscoring.scoring.Scorer._
     import com.dstatz.leadscoring.training.Trainer._
     import com.dstatz.leadscoring.processing.Processor._
     import com.dstatz.leadscoring.reporting.Reporter._
 
-    log.info("starting spark app")
-
-    val conf = ConfigFactory.load()
-    val factory = new AppFactory(conf)
+    log.info(s"starting spark app ${args.length}")
+    implicit val conf: Config = ConfigFactory.load()
+    implicit val factory: AppFactory = new AppFactory(conf)
+    implicit val session: SparkSession = factory.getSparkSession
 
     import factory._
     getSourceReader.readLeads
@@ -23,5 +25,8 @@ object Boot {
       .trainIfRequired()
       .score()
       .reportResults()
+
+    log.info("Closing Spark Session")
+    session.close()
   }
 }
--- lead-scoring/src/main/scala/com/dstatz/leadscoring/reporting/Reporter.scala
+++ lead-scoring/src/main/scala/com/dstatz/leadscoring/reporting/Reporter.scala
@@ -1,9 +1,13 @@
 package com.dstatz.leadscoring.reporting
 
 import org.apache.spark.sql._
+import com.typesafe.config.Config
 
 object Reporter {
-  implicit class Reporting(df: DataFrame) {
+  implicit class Reporting(df: DataFrame)(implicit
+      session: SparkSession,
+      conf: Config
+  ) {
     def reportResults(): Unit = ???
   }
 }
