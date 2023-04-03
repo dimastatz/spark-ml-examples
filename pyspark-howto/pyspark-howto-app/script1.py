395b3797a993101380c67767c0aeb2eb8f32bfdc
Dima Statz <dima.statz@gmail.com>
2023-04-03 21:53:31 +0300

    add pytest

--- pyspark-howto/pyspark-howto-app/script1.py
+++ pyspark-howto/pyspark-howto-app/script1.py
@@ -1,23 +1,23 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 
-spark = SparkSession.builder \
-    .appName("pyspark-howto-app-1") \
-    .config("spark.driver.bindAddress","127.0.0.1") \
-    .getOrCreate()
+def load_list_to_df() -> DataFrame:
+    spark = SparkSession.builder \
+        .appName("pyspark-howto-app-1") \
+        .config("spark.driver.bindAddress","127.0.0.1") \
+        .getOrCreate()
 
-data = [("Alice", 25), ("Bob", 30), ("Charlie", 35)]
+    data = [("Alice", 25), ("Bob", 30), ("Charlie", 35)]
 
-# Define the schema for the DataFrame
-schema = StructType([
-    StructField("name", StringType(), True),
-    StructField("age", IntegerType(), True)
-])
+    # Define the schema for the DataFrame
+    schema = StructType([
+        StructField("name", StringType(), True),
+        StructField("age", IntegerType(), True)
+    ])
 
-# Create the DataFrame from the list and the schema
-df = spark.createDataFrame(data, schema)
+    return spark.createDataFrame(data, schema)
 
-# Show the DataFrame
-df.show()
 
+if __name__ == "__main__":
+    load_list_to_df().show()
