4064f8489e2bc0fc6f30c4e8752dd3d1891b8407
Dima Statz <dima.statz@gmail.com>
2023-04-04 21:22:36 +0300

    add fixture

--- pyspark-howto/pyspark-howto-app/tests/test_crunch.py
+++ pyspark-howto/pyspark-howto-app/tests/test_crunch.py
@@ -1,7 +1,12 @@
 import cruncher
 from tests.common import spark
+from tests.fixtures.tenant_fixture import *
 
 
 def test_basic():
     df = cruncher.crunch(spark=spark)
     assert df == None
+
+
+def test_crunch(tenant):
+    assert tenant == 1
