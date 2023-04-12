b4a42447b8d30df07ae40c2b98a3050fba861516
dimastatz <dima.statz@gmail.com>
2023-04-12 21:07:47 +0300

    use monkeypatch

--- pyspark-howto/pyspark-howto-app/tests/test_script1.py
+++ pyspark-howto/pyspark-howto-app/tests/test_script1.py
@@ -1,11 +1,13 @@
-from script1 import load_list_to_df
 from tests.utils.tenant_fixture import *
 
 
-def test_always_passes():
-    assert True
+def test_always_passes(monkeypatch):
+    monkeypatch.setattr('script1.load_list_to_df', lambda x: 1)
+    from script1 import load_list_to_df
+    assert load_list_to_df(None) == 1
 
 
 def test_load_list_to_df(spark_session):
+    from script1 import load_list_to_df
     df = load_list_to_df(spark_session)
     assert df.count() == 3
