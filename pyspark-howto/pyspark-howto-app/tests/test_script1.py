47325f3b962090b77634ca4eaf63e0019fabda49
dimastatz <dima.statz@gmail.com>
2023-04-12 21:30:02 +0300

    add exception

--- pyspark-howto/pyspark-howto-app/tests/test_script1.py
+++ pyspark-howto/pyspark-howto-app/tests/test_script1.py
@@ -1,3 +1,4 @@
+import pytest
 from tests.utils.tenant_fixture import *
 
 
@@ -5,6 +6,18 @@ def test_always_passes(monkeypatch):
     monkeypatch.setattr('script1.load_list_to_df', lambda _: 1)
     from script1 import load_list_to_df
     assert load_list_to_df(None) == 1
+
+
+def test_raising_error(monkeypatch):
+    def raise_error(spark):
+        raise TypeError('error')
+     
+    monkeypatch.setattr('script1.load_list_to_df', raise_error)
+    from script1 import load_list_to_df
+    
+    with pytest.raises(TypeError) as e:
+        load_list_to_df(None)
+        assert str(e.value) == 'error'
 
 
 def test_load_list_to_df(spark_session):
