d80676d41feeefe04ab1b1e352298800b204639f
dimastatz <dima.statz@gmail.com>
2023-04-13 21:46:22 +0300

    add mongo to dict

--- pyspark-howto/pyspark-howto-app/tests/test_mongo.py
+++ pyspark-howto/pyspark-howto-app/tests/test_mongo.py
@@ -1,12 +1,10 @@
-import uuid
 import mongomock
-
 from models.lead import *
 from mongoengine import *
 from tests.utils.tenant_fixture import *
 
 
-def test_leads():
+def test_leads(spark_session):
     connect('mongoenginetest', 
             host='mongodb://localhost',
             mongo_client_class=mongomock.MongoClient, 
@@ -18,37 +16,41 @@ def test_leads():
     lead.save()
 
     first = SfdcLead.objects().first()
-    print(first.FirstName, first.LastName, dict(first.to_mongo()))
     assert first.FirstName == lead.FirstName
 
+    first_dic = dict(first.to_mongo())
+    del(first_dic['_id'])
+
+    df = spark_session.createDataFrame([first_dic])
+    assert df.count() == 1
+    
+    df.show()
     disconnect()
 
 
 def test_write(mongo_client):
     lead_in = { 'name': 'lead 1', 'company': 'lead 1 company' }
     lead_id = write(mongo_client, 'leads_db', 'leads', lead=lead_in)
-    print('lead_in', lead_id)
+    #print('lead_in', lead_id)
     lead_out = read(mongo_client, 'leads_db', 'leads', company='lead 1 company')
-    print('lead_out', lead_out[0], type(lead_out[0]))
+    #print('lead_out', lead_out[0], type(lead_out[0]))
     assert lead_in['name'] == lead_out[0]['name']
     assert lead_id == lead_out[0]['_id']
 
 
 def test_mongo_to_df(mongo_client, spark_session):
-    db_name = 'leads_db'
-    collection = 'leads'
-
+    # populate DB
     for i in range(1, 4):
         lead_in = { 'name': f'lead {i}', 'company': f'lead {i} company' }
-        write(mongo_client, db_name, collection, lead_in)
+        write(mongo_client, 'leads_db', 'leads', lead_in)
     
+    # fetch DB content
     leads = read(mongo_client, 'leads_db', 'leads', company='lead *')
     assert len(leads) == 4
 
     for lead in leads:
         del(lead['_id'])
 
-    print(leads)
     df = spark_session.createDataFrame(leads)
     df.show()
 
