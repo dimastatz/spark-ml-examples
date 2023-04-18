d9e771fbee62712c2c50964e465c7c32a3d799fa
dimastatz <dima.statz@gmail.com>
2023-04-18 21:06:56 +0300

    add session creation

--- pyspark-howto/pyspark-howto-app/cruncher.py
+++ pyspark-howto/pyspark-howto-app/cruncher.py
@@ -1,6 +1,13 @@
 from pyspark.sql import SparkSession, DataFrame
 
 
+def create_session(name='pyspark-example', host='127.0.0.1') -> SparkSession:
+    return SparkSession.builder \
+        .appName(name) \
+        .config('spark.driver.bindAddress', host) \
+        .getOrCreate()
+    
+
 def crunch(spark: SparkSession) -> DataFrame:
     return None
 
